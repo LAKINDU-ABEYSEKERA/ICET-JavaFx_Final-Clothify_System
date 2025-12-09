b549542b08166bfe69e4cf3f8be87cff7a428bf2
LAKINDU-ABEYSEKERA <2005nadu@gmail.com>
2025-12-09 08:35:39 +0530

    UserData Base Created and Login Form Successfully Implemented!..

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -2,16 +2,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 public class Starter extends Application {
-
     public static void main(String[] args) {
         launch();
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        // Load LoginForm instead of Mainboard
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/LoginForm.fxml"))));
         stage.show();
     }
--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -2,12 +2,18 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import model.entity.User;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import util.HibernateUtil;
+
+import java.io.IOException;
 
 public class LoginController {
 
@@ -15,31 +21,29 @@ public class LoginController {
     private TextField txtEmail;
 
     @FXML
-    private TextField txtPassword;
+    private PasswordField txtPass;
 
     @FXML
     void btnLoginOnAction(ActionEvent event) {
         String email = txtEmail.getText();
-        String password = txtPassword.getText();
+        String password = txtPass.getText();
 
         if (authenticate(email, password)) {
-            new Alert(Alert.AlertType.INFORMATION, "Login Successful!").show();
-
+            new Alert(Alert.AlertType.INFORMATION, "Login Successful!").showAndWait();
+            // We will add the Dashboard navigation here in the next step
         } else {
             new Alert(Alert.AlertType.ERROR, "Invalid Email or Password").show();
         }
     }
 
     private boolean authenticate(String email, String password) {
-
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-
+            // Check DB for user with matching email
             Query<User> query = session.createQuery("FROM User WHERE email = :e", User.class);
             query.setParameter("e", email);
-
             User user = query.uniqueResult();
 
-
+            // Check if password matches
             if (user != null && user.getPassword().equals(password)) {
                 return true;
             }
