6ab3db442b8d4b1e17c906e71d43c854dd6286a6
LAKINDU-ABEYSEKERA <2005nadu@gmail.com>
2025-12-13 21:58:36 +0530

    Login Form Design Improved

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.ScaleTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,7 +9,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import model.entity.User;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
@@ -18,10 +22,36 @@ import java.io.IOException;
 public class LoginController {
 
     @FXML
+    private AnchorPane root;
+
+    @FXML
     private TextField txtEmail;
 
     @FXML
     private PasswordField txtPass;
+
+    @FXML
+    public void initialize() {
+
+        // Safety check
+        if (root == null) {
+            System.out.println("ROOT IS NULL - fx:id missing!");
+            return;
+        }
+
+        FadeTransition fade = new FadeTransition(Duration.seconds(1), root);
+        fade.setFromValue(0);
+        fade.setToValue(1);
+
+        ScaleTransition scale = new ScaleTransition(Duration.seconds(0.8), root);
+        scale.setFromX(0.9);
+        scale.setFromY(0.9);
+        scale.setToX(1);
+        scale.setToY(1);
+
+        fade.play();
+        scale.play();
+    }
 
     @FXML
     void btnLoginOnAction(ActionEvent event) {
